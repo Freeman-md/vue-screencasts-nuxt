c1bfca3054a7fcfd6af986e9b74e069578d4e22e
Freeman-md <freemanmadudili@gmail.com>
2021-10-15 16:30:36 +0100

    setting up index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>
-    Index Page
+  <div class="home">
+    <div class="justify-center display-4 ma-4 d-flex">
+      All Videos
+    </div>
+
+    <div class="flex-wrap d-flex">
+      <div 
+        v-for="(video, index) in videos" 
+        :key="index">
+        {{ video.title }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  components: {},
+  async asyncData({ $axios }) {
+    let response = await $axios.get('/meals')
+    const videos = response.data.data
+
+    return {
+      videos
+    }
+  },
+}
 </script>
