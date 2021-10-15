01e24b314af1f1c15fdb6305af6505dd4262b00e
Freeman-md <freemanmadudili@gmail.com>
2021-10-15 10:54:00 +0100

    custom layouts

---
+++ layouts/theatre.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt id="theatre-body" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  #theatre-body {
+    background-color: black;
+    color: white;
+  }
+</style>
